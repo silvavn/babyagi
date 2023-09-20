8ab44885964d2707042a67f885c52dd960a6b725
Victor Silva <37382997+silvavn@users.noreply.github.com>
2023-09-19 18:54:50 -0600

    added api path documentation

--- baby_agi/__init_.py
+++ baby_agi/__init_.py
@@ -11,27 +11,10 @@ class BabyAGI:
                     OBJECTIVE (str): The objective of the agent.
                     INITIAL_TASK (str): The initial task of the agent.
                     LLM_MODEL (str): The LLM model alias being used to control the agent.
+                    LLAMA_API_PATH (str): The path to the llama api.
             """
             for key, value in kwargs.items():
                 setattr(self, key, value)
-
-    # function to set up chromadb client
-    @property
-    def vectordb_client(self):
-        return self._vectordb_client
-    
-    @vectordb_client.setter
-    def vectordb_client(self, value):
-        self._vectordb_client = value
-    
-    # function to set up LLM model
-    @property
-    def LLM_MODEL(self):
-        return self._LLM_MODEL
-    
-    @LLM_MODEL.setter
-    def LLM_MODEL(self, value):
-        self._LLM_MODEL = value
 
     # prints the BabyAGI configuration
     def print_config(self):
